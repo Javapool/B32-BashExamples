f2eccebc8bccacd7cf557a0bb9fd21cac3195a32
Louis-Chaels Deziel <lcdeziel@gmail.com>
2018-12-03 10:51:42 -0500

    boucles faites

--- 05-boucles.sh
+++ 05-boucles.sh
@@ -1,2 +1,24 @@
 #!/bin/bash
 
+for ((i=0;i<5;i++))
+do
+	echo $i
+done
+
+liste=`ls`
+
+
+# pour chaque element d'une liste...
+for fic in $liste
+do
+	echo $fic
+done
+
+resultat=o
+
+# -o or
+# -a and
+while test $resultat = o -o $resultat = O
+do
+	read -p "entrez o pour repeter la boucle : " resultat
+done
