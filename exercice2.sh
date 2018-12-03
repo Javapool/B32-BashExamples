e8228c1184432fff0508a893b9c9cb3e3ca9f516
Louis-Chaels Deziel <lcdeziel@gmail.com>
2018-12-03 12:14:34 -0500

    ex2fait

--- exercice2.sh
+++ exercice2.sh
@@ -1,1 +1,30 @@
 #!/bin/bash
+
+choix=0
+
+while [ $choix != "q" ]
+do
+	clear
+	echo "|--------------------------------------------------|"
+	echo "|--------------------------------------------------|"
+	echo "|                  =CVM Soft=                      |"
+	echo "|                                                  |"
+	echo "|               d = dictionnary                    |"
+	echo "|               q = quitter                        |"
+	echo "|--------------------------------------------------|"
+	read -p "Veuillez entrer votre choix : " choix
+	if [ $choix = "d" ]
+	then
+		./exercice1.sh
+		sleep 3
+
+	elif [ $choix = "q" ]
+	then
+		echo ""
+		echo "A U   R E V O I R !"
+		sleep 1
+	else
+		echo "Choix invalide. Veuillez recommencer"
+		sleep 1
+	fi
+done
