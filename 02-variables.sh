bb2eb761f22c2a797fbd401b079b033576eb1d8d
Louis-Chaels Deziel <lcdeziel@gmail.com>
2018-12-03 10:02:44 -0500

    02 rempli

--- 02-variables.sh
+++ 02-variables.sh
@@ -1,2 +1,22 @@
 #!/bin/bash
 
+prenom=Louis
+echo $prenom
+
+# Forcer une declaration en int
+declare -i age=33
+
+# Mettre une variable en read-only (const)
+declare -r age2=27
+
+#les variables systeme:
+
+#$0:nom de l'executable
+echo $0
+
+# $1-9 : arguments specifies dans lappel de l'executable
+echo $1
+echo $2
+
+# $#:nb d'arguments demandes
+echo $#
