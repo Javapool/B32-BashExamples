134f8eeb32e056c3ea3df34dcf433b862747f966
Louis-Chaels Deziel <lcdeziel@gmail.com>
2018-12-03 11:11:48 -0500

    regular expressions regularized

--- 08-regexp.sh
+++ 08-regexp.sh
@@ -1,2 +1,12 @@
 #!/bin/bash
 
+echo -n "Texte : "
+read texte
+
+# explications: =~ce qui suit est une expression reguliere. ^[0-9]+$ ce qui commence par et contient plus
+# d'un chiffre
+
+# ^=début; [x-y]=de x à y; +=au moins un; $= finit par;
+if [[ $texte =~ ^[0-9]+$ ]];then
+	echo "numerique"
+fi
