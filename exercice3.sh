3a26c949ddf46a60dab08fd8d4c4a79a8aeaf348
Louis-Chaels Deziel <lcdeziel@gmail.com>
2018-12-03 12:24:46 -0500

    exercice 3 complété

--- exercice3.sh
+++ exercice3.sh
@@ -1,1 +1,16 @@
 #!/bin/bash
+
+for ((i=1;i < 30;i++))
+do
+	for ((j=0;j < i; j++))
+	do
+		echo -n " "
+	done
+
+	echo -n "#"
+	sleep 0.075
+	clear
+done
+echo "                           B O O M !"
+sleep 3
+
