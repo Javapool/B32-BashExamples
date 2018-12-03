d6017def46e65a71ca353d90844575510fc28afc
Louis-Chaels Deziel <lcdeziel@gmail.com>
2018-12-03 10:09:51 -0500

    fini avec les calculs

--- 03-calculs.sh
+++ 03-calculs.sh
@@ -1,2 +1,14 @@
 #!/bin/bash
 
+# Mauvaise syntaxe: demande $ et deux parentheses
+echo 10+19
+
+echo $((10+19))
+
+read -p "Nombre 1:" premier
+read -p "Nombre 2:" second
+
+# let effectue un calcul demandé à l'aide de variables
+let resultat=$premier*$second
+
+echo $resultat
