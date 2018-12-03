c50dd492d3aa076ac3a487d26a5ea19099bdd412
Louis-Chaels Deziel <lcdeziel@gmail.com>
2018-12-03 09:49:55 -0500

    Ajout de code dans 01

--- 01-hello_world.sh
+++ 01-hello_world.sh
@@ -1,2 +1,15 @@
 #!/bin/bash
 
+# ceci est un commentaire
+
+echo "Hello world"
+echo -n " Vive "
+# le tiret n empeche lajout automatique d'un \n en fin de ligne
+echo "Linux"
+# le -e permer les caracteres speciaux comme \n
+echo -e "123\n123\n123"
+read -p "quel age avez-vous?" age
+#le -p permet d'ajouter un bloc texte avant le read. la variable est specifiee en parametre
+echo $age "ans! vouz avez l'air bien plus jeune!"
+# le $ est necessaire pour appeler une variable, mais pas pour assigner une valeur a la var.
+echo $PATH
